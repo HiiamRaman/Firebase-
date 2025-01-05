9e224f8e1937a298b5398b2462e8059cf8ca2c3c
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-01-05 16:31:17 +0545

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,76 @@
+  
+//1.the codes that are given below for realtime database
 
-import './App.css'
-import { getDatabase ,ref ,set} from "firebase/database";
-import {app} from './firebase'
+
+// import './App.css'
+// import { getDatabase ,ref ,set} from "firebase/database";
+// import {app} from './firebase'
  
-const db = getDatabase();
-function App() {
+// const db = getDatabase();
+// function App() {
 
-  function writeUserData() {
+//   function writeUserData() {
     
-    set(ref(db, 'users/Raman' ), {
-      name:"Raman Singh",
-      id : 10,
-      phonenumber:124324432,
-      email: 'abc@gmail.com'
+//     set(ref(db, 'users/Raman' ), {
+//       name:"Raman Singh Chauhan",
+//       id : 10,
+//       phonenumber:124324432,
+//       email: 'abc@gmail.com'
 
       
-    });
-  }
+//     });
+//   }
+
+
+
+//   return (
+//    <div>
+//     <h1>This web-app is for Firebase testing</h1>
+//     <button onClick={writeUserData}>Give me your info</button>
+//    </div>
+//   )
+// }
+
+// export default App
+    
+
+
+//2.the codes that are given below for Authentication
+
+
+
+import './App.css';
+import {app} from './firebase';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+const auth = getAuth() 
+
+
+import React from 'react'
+
+function App() {
+
+function Signup() {
   
+  createUserWithEmailAndPassword(auth,'RaRaftar@gmail.com', 'yoyobro')
+    .then((userCredential) => {
+console.log(userCredential);
+      
+    })
+    .catch((error) => {
+      console.log(error);
+      });  }
+  
+ 
 
 
 
 
   return (
-   <div>
-    <h1>This web-app is for Firebase testing</h1>
-    <button onClick={writeUserData}>Give me your info</button>
-   </div>
+    <div>
+
+      <button onClick={Signup}>CreateEmail</button>
+      
+    </div>
   )
 }
 
